2fc3c2c00594bd48e9c6a05100c9d77748454616
navendra3010 <sahunavendra1@gmail.com>
2025-03-28 11:22:20 +0530

    newupdate 28-3-25

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
     //   title: 'flutter Dark mode',
     //   //theme:
     //   // theme: ThemeData(
-    //   //   fontFamily: AppFont.fontFamily,
+    //   //   fontFamily: AppFont.fontFamily
     //   // ),
     //   theme: MyAppTheme.lightTheme,
     //   darkTheme: MyAppTheme.darkTheme,
